907355fd30d7631ea014917f53ae1f714ef003fc
kel918 <luozhk3@mail2.sysu.edu.cn>
2025-02-13 10:32:39 +0800

    feat: 基础层持久化数据

---
+++ big-market-infrastructure/src/main/java/org/example/infrastructure/persistent/dao/IStrategyAwardDao.java
@@ -0,0 +1,17 @@
+package org.example.infrastructure.persistent.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.example.infrastructure.persistent.po.StrategyAward;
+
+import java.util.List;
+
+/**
+ * 抽奖策略奖品明细配置 - 概率、规则 DAO
+ */
+@Mapper
+public interface IStrategyAwardDao {
+
+    List<StrategyAward> queryStrategyAwardList();
+
+}
+
